8c315db90b7fdab9226eb2a8a6e2c7af593fb714
Luiz Kim <luiz.kim@controleonline.com>
2024-04-21 12:33:29 -0300

    Update

--- src/Service/PeopleService.php
+++ src/Service/PeopleService.php
@@ -5,6 +5,7 @@ namespace ControleOnline\Service;
 use ControleOnline\Entity\Language;
 use ControleOnline\Entity\People;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Security;
 
 class PeopleService
@@ -13,12 +14,19 @@ class PeopleService
   public function __construct(
     private EntityManagerInterface $manager,
     private Security               $security,
-
+    private RequestStack $requestStack
   ) {
   }
 
   public function beforePersist(People $people)
   {
+
+
+    $request = $this->requestStack->getCurrentRequest();
+
+    if ($request) {
+      print_r($request->request->all());
+    }
     $language = $this->manager->getRepository(Language::class)->findOneBy(['language' => 'pt-br']);
     $people->setLanguage($language);
     return $people;
