48a9625c46b776792fba5fc0404831c9d14c6fc2
Luiz Kim <luiz.kim@controleonline.com>
2024-06-26 14:21:12 -0300

    Update

--- src/Repository/ExtraFieldsRepository.php
+++ src/Repository/ExtraFieldsRepository.php
@@ -2,7 +2,7 @@
 
 namespace ControleOnline\Repository;
 
-use ControleOnline\Entity\ExtraFields\ExtraFields;
+use ControleOnline\Entity\ExtraFields;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
