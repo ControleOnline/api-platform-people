85213672153ae41cffb00100b54b86e4c12a41c0
Luiz Kim <luiz.kim@controleonline.com>
2024-06-26 12:03:59 -0300

    Update

--- src/Entity/Particulars/Particulars.php
+++ src/Entity/Particulars/Particulars.php
@@ -9,12 +9,25 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+
 /**
  * @ORM\EntityListeners ({ControleOnline\Listener\LogListener::class})
  * @ORM\Table (name="particulars")
  * @ORM\Entity (repositoryClass="ControleOnline\Repository\ParticularsRepository")
  */
-#[ApiResource(operations: [new Get(security: 'is_granted(\'ROLE_CLIENT\')'), new GetCollection(security: 'is_granted(\'ROLE_CLIENT\')')], formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']], normalizationContext: ['groups' => ['particulars_read']], denormalizationContext: ['groups' => ['particulars_write']])]
+#[ApiResource(
+    operations: [
+        new Get(security: 'is_granted(\'ROLE_CLIENT\')'),
+        new GetCollection(security: 'is_granted(\'ROLE_CLIENT\')')
+    ],
+    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' =>
+    ['text/csv']],
+    normalizationContext: ['groups' => ['particulars_read']],
+    denormalizationContext: ['groups' => ['particulars_write']]
+)]
+#[ApiFilter(filterClass: SearchFilter::class, properties: ['id' => 'exact', 'type' => 'exact', 'people' => 'exact'])]
+
 class Particulars
 {
     /**
@@ -22,6 +35,7 @@ class Particulars
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
+     * @Groups({"particularstype_read", "particulars_read"})
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
@@ -64,30 +78,30 @@ class Particulars
     {
         return $this->id;
     }
-    public function setType(ParticularsType $type) : self
+    public function setType(ParticularsType $type): self
     {
         $this->type = $type;
         return $this;
     }
-    public function getType() : ParticularsType
+    public function getType(): ParticularsType
     {
         return $this->type;
     }
-    public function setPeople(People $people) : self
+    public function setPeople(People $people): self
     {
         $this->people = $people;
         return $this;
     }
-    public function getPeople() : People
+    public function getPeople(): People
     {
         return $this->people;
     }
-    public function setValue(string $value) : self
+    public function setValue(string $value): self
     {
         $this->value = $value;
         return $this;
     }
-    public function getValue() : string
+    public function getValue(): string
     {
         return $this->value;
     }
--- src/Entity/Particulars/ParticularsType.php
+++ src/Entity/Particulars/ParticularsType.php
@@ -10,13 +10,22 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+
 /**
  * @ORM\EntityListeners ({ControleOnline\Listener\LogListener::class})
  * @ORM\Table (name="particulars_type")
  * @ORM\Entity (repositoryClass="ControleOnline\Repository\ParticularsTypeRepository")
  */
-#[ApiResource(operations: [new Get(security: 'is_granted(\'ROLE_CLIENT\')'), new GetCollection(security: 'is_granted(\'ROLE_CLIENT\')')], formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']], normalizationContext: ['groups' => ['particularstype_read']], denormalizationContext: ['groups' => ['particularstype_write']])]
-#[ApiFilter(filterClass: SearchFilter::class, properties: ['peopleType' => 'exact', 'context' => 'partial', 'fieldType' => 'exact'])]
+#[ApiResource(
+    operations: [
+        new Get(security: 'is_granted(\'ROLE_CLIENT\')'),
+        new GetCollection(security: 'is_granted(\'ROLE_CLIENT\')')
+    ],
+    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
+    normalizationContext: ['groups' => ['particularstype_read']],
+    denormalizationContext: ['groups' => ['particularstype_write']]
+)]
+#[ApiFilter(filterClass: SearchFilter::class, properties: ['peopleType' => 'exact', 'context' => 'exact', 'fieldType' => 'exact'])]
 class ParticularsType
 {
     /**
@@ -24,6 +33,7 @@ class ParticularsType
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
+     * @Groups({"particularstype_read", "particulars_read"})
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
@@ -72,57 +82,57 @@ class ParticularsType
     {
         return $this->id;
     }
-    public function setPeopleType(string $type) : self
+    public function setPeopleType(string $type): self
     {
         $this->peopleType = $type;
         return $this;
     }
-    public function getPeopleType() : string
+    public function getPeopleType(): string
     {
         return $this->peopleType;
     }
-    public function setTypeValue(string $value) : self
+    public function setTypeValue(string $value): self
     {
         $this->typeValue = $value;
         return $this;
     }
-    public function getTypeValue() : string
+    public function getTypeValue(): string
     {
         return $this->typeValue;
     }
-    public function setFieldType(string $value) : self
+    public function setFieldType(string $value): self
     {
         $this->fieldType = $value;
         return $this;
     }
-    public function getFieldType() : string
+    public function getFieldType(): string
     {
         return $this->fieldType;
     }
-    public function setContext(string $value) : self
+    public function setContext(string $value): self
     {
         $this->context = $value;
         return $this;
     }
-    public function getContext() : string
+    public function getContext(): string
     {
         return $this->context;
     }
-    public function setRequired(string $value) : self
+    public function setRequired(string $value): self
     {
         $this->required = $value;
         return $this;
     }
-    public function getRequired() : ?string
+    public function getRequired(): ?string
     {
         return $this->required;
     }
-    public function setFieldConfigs(string $value) : self
+    public function setFieldConfigs(string $value): self
     {
         $this->fieldConfigs = $value;
         return $this;
     }
-    public function getFieldConfigs() : ?string
+    public function getFieldConfigs(): ?string
     {
         return $this->fieldConfigs;
     }
