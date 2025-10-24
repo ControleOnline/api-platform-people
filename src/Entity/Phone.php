c12b244fe51d71611f928bfee753c3c7ab41e1da
Alexandre de Silva Cunha <alemac@mac.com>
2025-10-24 12:47:45 -0300

    inclui a funcionalidade de deletar o telefone

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -24,7 +24,8 @@ use Doctrine\ORM\Mapping as ORM;
             validationContext: ['groups' => ['phone:read']],
             denormalizationContext: ['groups' => ['phone:write']]
         ),
-        new Post(securityPostDenormalize: 'is_granted(\'ROLE_CLIENT\')')
+        new Post(securityPostDenormalize: 'is_granted(\'ROLE_CLIENT\')'),
+        new Delete(security: "is_granted('ROLE_CLIENT')")
     ],
     formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
     normalizationContext: ['groups' => ['phone:read']],
