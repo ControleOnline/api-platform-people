3b7fb4db0793a5d182b5f2aef7fcbbe6c044772b
Luiz Kim <luiz.kim@controleonline.com>
2026-03-07 09:20:06 -0300

    Salesman

--- src/Service/SalesmanService.php
+++ src/Service/SalesmanService.php
@@ -5,22 +5,17 @@ namespace ControleOnline\Service;
 use ControleOnline\Entity\People;
 use ControleOnline\Entity\PeopleLink;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface as Security;
 use ControleOnline\Event\EntityChangedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class SalesmanService implements EventSubscriberInterface
 {
-  private $request;
 
   public function __construct(
     private EntityManagerInterface $manager,
-    private Security $security,
-    private RequestStack $requestStack,
-  ) {
-    $this->request = $requestStack->getCurrentRequest();
-  }
+    private Security $security
+  ) {}
 
   public static function getSubscribedEvents(): array
   {
@@ -31,25 +26,35 @@ class SalesmanService implements EventSubscriberInterface
 
   public function onEntityChanged(EntityChangedEvent $event)
   {
-    $oldEntity = $event->getOldEntity();
     $entity = $event->getEntity();
 
-    if (!$entity instanceof People || !$oldEntity instanceof People) {
+    if (!$entity instanceof PeopleLink) {
       return;
     }
 
-    foreach ($entity->getLink() as $link) {
-      if ($link->getLinkType() === 'client') {
-        $this->discoverSalesmanForClient(
-          $link->getCompany(),
-          $link->getPeople()
-        );
-      }
+    // reagir apenas quando empresa vira cliente
+    if ($entity->getLinkType() !== 'client') {
+      return;
     }
+
+    $company = $entity->getCompany();
+    $client = $entity->getPeople();
+
+    // evita rodar em vendedor -> cliente
+    if ($this->isSalesman($company)) {
+      return;
+    }
+
+    $this->discoverSalesmanForClient($company, $client);
   }
 
   public function discoverSalesmanForClient(People $company, People $client): ?People
   {
+
+    // cliente já tem vendedor dessa empresa?
+    if ($this->clientAlreadyHasSalesmanFromCompany($company, $client)) {
+      return null;
+    }
 
     $salesman = $this->getSalesmanFromCompany(
       $company,
@@ -60,48 +65,64 @@ class SalesmanService implements EventSubscriberInterface
       return null;
     }
 
-    // evita duplicação
-    $exists = $this->manager->getRepository(PeopleLink::class)->findOneBy([
-      'company' => $salesman,
-      'people' => $client,
-      'link_type' => 'client'
-    ]);
+    $link = new PeopleLink();
+    $link->setCompany($salesman);
+    $link->setPeople($client);
+    $link->setLinkType('client');
 
-    if ($exists) {
-      return $salesman;
-    }
-
-    $peopleLink = new PeopleLink();
-    $peopleLink->setCompany($salesman);
-    $peopleLink->setPeople($client);
-    $peopleLink->setLinkType('client');
-
-    $this->manager->persist($peopleLink);
+    $this->manager->persist($link);
 
     return $salesman;
   }
 
+  private function clientAlreadyHasSalesmanFromCompany(People $company, People $client): bool
+  {
+
+    $result = $this->manager->getRepository(PeopleLink::class)
+      ->createQueryBuilder('clientLink')
+      ->join(
+        PeopleLink::class,
+        'salesmanLink',
+        'WITH',
+        'salesmanLink.people = clientLink.company AND salesmanLink.link_type = :salesman'
+      )
+      ->andWhere('clientLink.people = :client')
+      ->andWhere('clientLink.link_type = :clientType')
+      ->andWhere('salesmanLink.company = :company')
+      ->setParameter('client', $client)
+      ->setParameter('company', $company)
+      ->setParameter('clientType', 'client')
+      ->setParameter('salesman', 'salesman')
+      ->setMaxResults(1)
+      ->getQuery()
+      ->getOneOrNullResult();
+
+    return $result !== null;
+  }
+
   public function getMyPeople(): ?People
   {
+
     $token = $this->security->getToken();
 
     if (!$token) {
       return null;
     }
 
-    $currentUser = $token->getUser();
+    $user = $token->getUser();
 
-    if (!$currentUser) {
+    if (!$user || !method_exists($user, 'getPeople')) {
       return null;
     }
 
-    return $currentUser->getPeople();
+    return $user->getPeople();
   }
 
-  public function getSalesmanFromCompany(People $company, ?People $people): ?People
+  public function getSalesmanFromCompany(People $company, ?People $me): ?People
   {
 
-    if ($people) {
+    // se eu sou vendedor da empresa, usar eu
+    if ($me) {
 
       $result = $this->manager->getRepository(PeopleLink::class)
         ->createQueryBuilder('pl')
@@ -109,17 +130,18 @@ class SalesmanService implements EventSubscriberInterface
         ->andWhere('pl.people = :people')
         ->andWhere('pl.link_type = :type')
         ->setParameter('company', $company)
-        ->setParameter('people', $people)
+        ->setParameter('people', $me)
         ->setParameter('type', 'salesman')
         ->setMaxResults(1)
         ->getQuery()
         ->getOneOrNullResult();
 
       if ($result) {
-        return $result->getPeople();
+        return $me;
       }
     }
 
+    // pegar vendedor aleatório da empresa
     $result = $this->manager->getRepository(PeopleLink::class)
       ->createQueryBuilder('pl')
       ->andWhere('pl.company = :company')
@@ -132,5 +154,19 @@ class SalesmanService implements EventSubscriberInterface
       ->getOneOrNullResult();
 
     return $result?->getPeople();
+  }
+
+  private function isSalesman(People $people): bool
+  {
+
+    return (bool) $this->manager->getRepository(PeopleLink::class)
+      ->createQueryBuilder('pl')
+      ->andWhere('pl.people = :people')
+      ->andWhere('pl.link_type = :type')
+      ->setParameter('people', $people)
+      ->setParameter('type', 'salesman')
+      ->setMaxResults(1)
+      ->getQuery()
+      ->getOneOrNullResult();
   }
 }
