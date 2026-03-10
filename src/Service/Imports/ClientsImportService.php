e2557d2d0c01935dc35686122d55fd683c768241
Luiz Kim <luiz.kim@controleonline.com>
2026-03-10 11:15:33 -0300

    Leads

---
+++ src/Service/Imports/ClientsImportService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ControleOnline\Service\Imports;
+
+class LeadsImportService extends PeopleImportService
+{
+    public function __construct() {}
+
+    public function getType(): string
+    {
+        return 'clients';
+    }
+}
