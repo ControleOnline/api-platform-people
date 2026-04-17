a14767f6b5296f25bec3c9550300c4bb2bf2a5e6
Luiz Kim <luiz.kim@controleonline.com>
2026-04-17 10:14:03 -0300

    Refatora CreateAccountAction para utilizar AccountRegistrationService e simplifica o registro de contas

--- src/Controller/CreateAccountAction.php
+++ src/Controller/CreateAccountAction.php
@@ -3,109 +3,24 @@
 namespace ControleOnline\Controller;
 
 use ControleOnline\Entity\People;
-use ControleOnline\Entity\User;
-use ControleOnline\Service\DomainService;
-use Doctrine\ORM\EntityManagerInterface;
+use ControleOnline\Service\AccountRegistrationService;
 use ControleOnline\Service\HydratorService;
-use ControleOnline\Service\PeopleLinkService;
-use ControleOnline\Service\PeopleService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use ControleOnline\Service\UserService;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class CreateAccountAction
 {
   public function __construct(
-    private EntityManagerInterface $manager,
-    private UserService $userService,
-    private PeopleService $peopleService,
-    private PeopleLinkService $peopleLinkService,
+    private AccountRegistrationService $accountRegistrationService,
     private HydratorService $hydratorService,
-    private DomainService $domainService
   ) {}
 
   public function __invoke(Request $request)
   {
     try {
-
-      $payload = json_decode($request->getContent());
-
-      if (!$payload || !isset($payload->people))
-        throw new BadRequestHttpException('people is required');
-
-      $peopleData = $payload->people;
-
-      if (
-        !isset($peopleData->name) ||
-        !isset($peopleData->alias) ||
-        !isset($peopleData->email) ||
-        !isset($peopleData->phone)
-      )
-        throw new BadRequestHttpException('name, alias, email and phone are required');
-
-      if (
-        !isset($peopleData->phone->ddi) ||
-        !isset($peopleData->phone->ddd) ||
-        !isset($peopleData->phone->phone)
-      )
-        throw new BadRequestHttpException('phone.ddi, phone.ddd and phone.number are required');
-
-      $people = $this->peopleService->discoveryPeople(
-        $peopleData->document ?? null,
-        $peopleData->email,
-        (array)$peopleData->phone,
-        $peopleData->name . ' ' . $peopleData->alias,
-        'F'
+      $client = $this->accountRegistrationService->registerFromContent(
+        $request->getContent()
       );
-
-      $company = null;
-
-      if (isset($payload->company)) {
-
-        $companyData = $payload->company;
-
-        $company = $this->peopleService->discoveryPeople(
-          $companyData->document ?? null,
-          $companyData->email ?? null,
-          $companyData->phone ?? null,
-          $companyData->name,
-          'J'
-        );
-
-        $this->peopleService->discoveryLink(
-          $company,
-          $people,
-          'employee'
-        );
-
-        $client = $company;
-      } else {
-        $client = $people;
-      }
-
-      $mainCompany = $this->domainService->getPeopleDomain()->getPeople();
-
-      $this->peopleService->discoveryLink(
-        $mainCompany,
-        $client,
-        'client'
-      );
-
-      if (isset($peopleData->user)) {
-
-        if (
-          !isset($peopleData->user->user) ||
-          !isset($peopleData->user->password)
-        )
-          throw new BadRequestHttpException('user.user and user.password are required');
-
-        $this->userService->createUser(
-          $people,
-          $peopleData->user->user,
-          $peopleData->user->password
-        );
-      }
 
       return new JsonResponse(
         $this->hydratorService->item(
--- src/Controller/DeleteUserAction.php
+++ src/Controller/DeleteUserAction.php
@@ -3,46 +3,22 @@
 namespace ControleOnline\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
-as Security;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
 use ControleOnline\Entity\People;
-use ControleOnline\Entity\User;
+use ControleOnline\Service\UserService;
 
 
 class DeleteUserAction
 {
-    /**
-     * Entity Manager
-     *
-     * @var EntityManagerInterface
-     */
-    private $manager = null;
-
-    /**
-     * Request
-     *
-     * @var Request
-     */
-    private $request  = null;
-
-
-
-    public function __construct(EntityManagerInterface $manager)
-    {
-        $this->manager     = $manager;
-    }
+    public function __construct(private UserService $userService) {}
 
     public function __invoke(People $data, Request $request): JsonResponse
     {
-        $this->request = $request;
-
         try {
-            $payload   = json_decode($this->request->getContent(), true);
-            $result    = $this->deleteUser($data, $payload);
+            $result = $this->userService->deleteUserFromContent(
+                $data,
+                $request->getContent()
+            );
 
             return new JsonResponse([
                 'response' => [
@@ -62,40 +38,6 @@ class DeleteUserAction
                     'success' => false,
                 ],
             ]);
-        }
-    }
-
-
-    private function deleteUser(People $person, array $payload): bool
-    {
-        try {
-            $this->manager->getConnection()->beginTransaction();
-
-            if (!isset($payload['id'])) {
-                throw new \InvalidArgumentException('Document id is not defined');
-            }
-
-            $users   = $this->manager->getRepository(User::class)->findBy(['people' => $person]);
-            if (count($users) == 1) {
-                throw new \InvalidArgumentException('Deve existir pelo menos um usuário');
-            }
-
-            $user    = $this->manager->getRepository(User::class)->findOneBy(['id' => $payload['id'], 'people' => $person]);
-            if (!$user instanceof User) {
-                throw new \InvalidArgumentException('Person user was not found');
-            }
-
-            $this->manager->remove($user);
-
-            $this->manager->flush();
-            $this->manager->getConnection()->commit();
-
-            return true;
-        } catch (\Exception $e) {
-            if ($this->manager->getConnection()->isTransactionActive())
-                $this->manager->getConnection()->rollBack();
-
-            throw new \InvalidArgumentException($e->getMessage());
         }
     }
 }
