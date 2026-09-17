3b8e710c9b7a39f589c6d621d82f003f458ce13d
leandro <leandro.jareno@gmail.com>
2026-09-16 23:14:49 -0300

    fix(task-109): enforce company administrative authority

--- src/Service/PeopleRoleService.php
+++ src/Service/PeopleRoleService.php
@@ -75,7 +75,7 @@ class PeopleRoleService
 
             if (
                 $this->isMainCompany($company)
-                && $link->getLinkType() === 'owner'
+                && in_array($link->getLinkType(), PeopleLink::ADMIN_LINK, true)
             ) {
                 $roles[] = 'ROLE_SUPER';
             }
@@ -192,12 +192,10 @@ class PeopleRoleService
             }
 
             $permissions[] = (string) $link->getLinkType();
-            if (
-                $this->isMainCompany($linkedCompany)
-                && $link->getLinkType() === 'owner'
-            ) {
-                $permissions[] = 'super';
-            }
+        }
+
+        if ($this->isSuperAdmin($people)) {
+            $permissions[] = 'super';
         }
 
         $permissions = array_merge(
@@ -212,6 +210,19 @@ class PeopleRoleService
         }
 
         return $this->companyPermissionsCache[$cacheKey] = $permissions;
+    }
+
+    public function isSuperAdmin(?People $people = null): bool
+    {
+        return in_array('ROLE_SUPER', $this->getGrantedRoles($people), true);
+    }
+
+    public function canAdministerCompany(People $company, ?People $people = null): bool
+    {
+        return array_intersect(
+            [...PeopleLink::ADMIN_LINK, 'super'],
+            $this->getCompanyPermissions($company, $people)
+        ) !== [];
     }
 
     public function getAllRoles(People $people): array
